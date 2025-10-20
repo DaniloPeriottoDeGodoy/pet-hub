4cac245ff774cc5a4a86925fbe468b2405d9e658
DaniloPeriottoDeGodoy <daniloperiottodegodoy@yahoo.com.br>
2025-10-20 19:49:57 -0300

    Refactor test. Change name to 'Should_Validate_Pet_Name'

--- PetHub.Domain.Tests/Entities/PetTests.cs
+++ PetHub.Domain.Tests/Entities/PetTests.cs
@@ -2,6 +2,7 @@
 using PetHub.Domain.Entities;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,14 +21,17 @@ namespace PetHub.Domain.Tests.Entities
         }
 
         [Test]
-        public void Pet_Should_Have_Name()
+        public void Should_Validate_Pet_Name()
         {
             // Arrange and Act
             _pet = new Pet(null);
 
             // Assert
-            Assert.That(_pet.IsInvalid, Is.True);
-            Assert.That(_pet.Errors.Contains("Pet name is invalid"), Is.True);
+            Assert.Multiple(() =>
+            {
+                Assert.That(_pet.IsInvalid, Is.True);
+                Assert.That(_pet.Errors.Contains("Pet name is invalid"), Is.True);
+            });
         }
     }
 }
