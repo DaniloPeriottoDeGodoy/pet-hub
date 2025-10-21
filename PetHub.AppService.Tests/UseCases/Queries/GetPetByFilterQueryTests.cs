390cdba12f82ec5000c24ab167db84baf468f1db
DaniloPeriottoDeGodoy <daniloperiottodegodoy@yahoo.com.br>
2025-10-21 20:35:15 -0300

    [red] Should_Get_Pet_Filtering_By_Specie

--- PetHub.AppService.Tests/UseCases/Queries/GetPetByFilterQueryTests.cs
+++ PetHub.AppService.Tests/UseCases/Queries/GetPetByFilterQueryTests.cs
@@ -51,5 +51,36 @@ namespace PetHub.AppService.Tests.UseCases.Queries
                 x => x.GetByFilterAsync(It.IsAny<string>()), Times.Once()
             );
         }
+
+        [Test]
+        public async Task Should_Get_Pet_Filtering_By_Specie()
+        {
+            // Arrange            
+            var specieSearch = Species.Dog;
+            var query = new GetPetByFilterQuery(null, specieSearch);
+
+            var listOfPetsFound = new List<Pet>
+            {
+                new Pet("Buddy", Species.Dog) { Id = Guid.NewGuid() },
+                new Pet("Puppy", Species.Dog) { Id = Guid.NewGuid() },
+                new Pet("Bilu", Species.Dog) { Id = Guid.NewGuid() }
+            };
+
+            _petRepository
+                .Setup(x => x.GetByFilterAsync(null, It.IsAny<Species>()))
+                .ReturnsAsync(Result.Ok(listOfPetsFound));
+
+            // Act
+            Result<List<Pet>> result = await _handler.Handle(query);
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.IsSuccess, Is.True);
+
+            _petRepository.Verify
+            (
+                x => x.GetByFilterAsync(It.IsAny<string>(), It.IsAny<Species>()), Times.Once()
+            );
+        }
     }
 }
